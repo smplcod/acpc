ff17cd96b2c356cb60bcc86ff3385db9019da954
Mihail Frontend <87027979+smplcod@users.noreply.github.com>
2025-08-31 13:15:50 +0600

    Update subPages.jsx

--- src/admin/app/subPages.jsx
+++ src/admin/app/subPages.jsx
@@ -31,7 +31,7 @@ export default function SubPages() {
   if (children.length === 0) return null
   return (
     <div className="subpages">
-      <h2>Subpages:</h2>
+      <h2>Subpages</h2>
       {renderTree(children)}
     </div>
   )
